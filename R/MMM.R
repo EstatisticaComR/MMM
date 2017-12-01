a07fb24c63eef84c3fb3de82a2c4a6379e72abe7
EstatisticaComR <estatisticacomruff@gmail.com>
2017-12-01 16:36:30 -0200

    Criação

---
+++ R/MMM.R
@@ -0,0 +1,15 @@
+#'MMM
+#'
+#'Calcula média e mediana de um vetor
+#'
+#'Função ruim
+#'
+#'@param x que é um vetor
+#'
+#'@return um vetor com a média e mediana
+#'
+#'@export 
+#'
+MMM = function(x){
+  return(c(mean(x),median(x)))
+}
